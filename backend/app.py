daa655aef4a2967ef46416e3aad3b0c112e635d4
MisganaGetachew <misganagetachew111@gmail.com>
2024-11-02 13:58:32 +0300

    hide mongo uri

--- backend/app.py
+++ backend/app.py
@@ -5,9 +5,10 @@ from pymongo.mongo_client import MongoClient
 from pydantic import BaseModel
 from bson.json_util import dumps, loads
 from fastapi.responses import JSONResponse
+from mongo import mongouri
 
 app = FastAPI()
-uri = "mongodb+srv://misganagetachew:test1234@cluster0.y9koy.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+uri = mongouri
 client = MongoClient(uri, server_api=ServerApi('1'))
 db = client["Links"]
 collection = db["link-info"]

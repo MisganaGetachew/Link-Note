1c356bcd1a8e5bb43087b2cefeff4b64fa64caf7
MisganaGetachew <misganagetachew111@gmail.com>
2024-11-02 13:48:31 +0300

    crud completed

--- backend/app.py
+++ backend/app.py
@@ -1,33 +1,60 @@
 from fastapi import FastAPI
-import requests
+# import requests
+from pymongo.server_api import ServerApi
+from pymongo.mongo_client import MongoClient
 from pydantic import BaseModel
+from bson.json_util import dumps, loads
+from fastapi.responses import JSONResponse
+
 app = FastAPI()
+uri = "mongodb+srv://misganagetachew:test1234@cluster0.y9koy.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+client = MongoClient(uri, server_api=ServerApi('1'))
+db = client["Links"]
+collection = db["link-info"]
 
 
 class Link(BaseModel):
-    social_Media: str = None
+    social_media: str = None
     link: str = None
     Note: str = None
 
 
 @app.get('/')
-def home():
+async def home():
 
     return {"message": "home is working properlly"}
 
 
 @app.get('/check')
-def check():
+async def check():
 
     return {"message": "this is a check message for deployment testing"}
 
 
 @app.get('/links')
-def get_links(url, note):
+async def get_links():
+    try:
+        # Retrieve and process documents
+        data = collection.find({})
+        documents = []
+        for doc in data:
+            doc.pop("_id", None)  # Remove _id field if it exists
+            documents.append(doc)
 
-    return {"Note": note, "url": url}
+        if not documents:
+            return {"message": "No data found"}
+        return {"links": documents}
+    except Exception as e:
+        return {"error occurred": str(e)}
 
 
 @app.post('/save-links')
-async def save_link(links: Link):
-    return {"message": "it is working", "data": links}
+async def save_link(links: Link):  # update/create
+
+    try:
+        collection.insert_one(links)
+        print("create operation succssful")
+        return {"message": "your link is saved propoerlly", "hear is the data saved for reference": links}
+    except:
+        print(Exception)
+        return {"error": Exception}
